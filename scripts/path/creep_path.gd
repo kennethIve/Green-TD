9955a2726dc4d0f8fc78f22b98b16c4a14255df3
kennethIve <kenneth96@outlook.com>
2026-09-07 15:20:57 +0800

    HUD polish, 4-corner spawns, multi-tower upgrades

--- scripts/path/creep_path.gd
+++ scripts/path/creep_path.gd
@@ -1,10 +1,10 @@
 extends Node2D
-## Waypoint path for creeps. Children Marker2D = path points in order.
+## Waypoint path. Marker2D children = points in order.
 class_name CreepPath
 
 func get_points() -> PackedVector2Array:
 	var pts: PackedVector2Array = []
 	for c in get_children():
-		if c is Node2D:
-			pts.append((c as Node2D).global_position)
+		if c is Marker2D:
+			pts.append((c as Marker2D).global_position)
 	return pts
